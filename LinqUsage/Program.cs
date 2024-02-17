8e888126f91a118331e4c88ddae5fb1bfc35888f
EHOKEHTY <jenya2003a@gmail.com>
2024-02-17 13:12:48 +0200

    Добавьте файлы проекта.

---
+++ LinqUsage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Headers;
+using System.Runtime.Intrinsics.Arm;
+
+namespace LinqUsage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
